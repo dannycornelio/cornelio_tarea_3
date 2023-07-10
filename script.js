70655d52e6da895144960b66c36d293cbf220c63
dannycornelio <licdannycornelio@gmail.com>
2023-07-10 10:13:47 -0600

    creacion del evento addEventListener con el keydown

--- script.js
+++ script.js
@@ -46,16 +46,39 @@
 // })
 
 
-//se procede a colocar un color aleatorio al boton
+//selecionamos el elemento button dentro de la constante boton
+const boton = document.querySelector("button");
+
+//Esta línea de código define una función llamada "random" que toma un parámetro llamado "numero". 
+//Utiliza la función Math.random() para generar un número decimal aleatorio entre 0 y 1, 
+//luego lo multiplica por el parámetro "numero" más uno y finalmente utiliza Math.floor() 
+//para redondear el resultado al número entero más cercano. El resultado final es un número aleatorio entre 0 y "numero".
+const random = (number) => Math.floor(Math.random() * (number +1));
+
+// Cuando se hace clic en el botón, se genera un color de fondo aleatorio y se aplica al elemento body
+boton.addEventListener("click", () =>{
+    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    document.body.style.backgroundColor = rndCol;
+});
 
 
-// const randomColor =(number) => {
-//     return Math.floor (Math.random() * number + 1)
-// }
 
-// button.addEventListener('click', (e) => {
-//     console.log(e)
-//     document.body.style.backgroundColor = `rgb(${randomColor(255)}, ${randomColor(255)}, ${randomColor(255)})
-// `})
+
+// selecciona un elemento de entrada (input) con el id "inputTex"
+const inputTex = document.querySelector('#inputTex');
+
+//selecciona un elemento con el id "output"
+const output = document.querySelector('#output');
+
+//Cuando se presiona una tecla, se actualiza el contenido del elemento "output" con el texto que indica la tecla presionada
+inputTex.addEventListener(
+    "keydown",
+    (e) => (output.textContent = `Usted ha presionado la tecla: "${e.key}".`)
+);
+
+
+
+
+
 
 
